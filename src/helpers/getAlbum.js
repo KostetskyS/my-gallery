c5982ecd7e54b858d57e4b0269d6137a07899a17
KostetskyS <kostetsky1337@gmail.com>
2023-04-23 12:18:01 +0300

    minor

---
+++ src/helpers/getAlbum.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+export const getAlbum = async (url) => {
+
+  const token = localStorage.getItem('authToken');
+
+  return await axios(`http://localhost:8080/api${url}`, {
+    headers: {
+      'Authorization' : 'Bearer ' + token
+    },
+    method: 'GET',
+  })
+
+}
+  
+  
--- src/views/Albums.js
+++ src/views/Albums.js
@@ -2,48 +2,37 @@ import React from 'react';
 import '../assets/style/main.css';
 import CreatePhoto from '../components/CreatePhoto';
 import { Container, Row } from 'react-bootstrap';
-import axios from 'axios';
-
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { getAlbum } from '../helpers/getAlbum';
 
 export const Albums = () => {
   const [album, setAlbum] = useState('');
 
-  async function showAlbums(data) {
-    const token = localStorage.getItem('authToken');
-    await axios('http://localhost:8080/api/albums', {
-      headers: {
-        'Authorization' : 'Bearer ' + token
-      },
-      method: 'GET',
-    })
-    .then(async function(response)  {
-      // const url = response.data.data[0].title;  оно работало
-      const albums = response.data.data.map((album) => {
-        // console.log(album, 'album')
-        return (  
-          <div key={album._id}> 
-            <h3>{album.title}</h3>
-            <h1>{album.description}</h1>
-           <img width='50px' height='50px' src={album.image.img_link}/>
-          </div>
-        )
-      })
-       
-      setAlbum(albums); 
-      // console.log(url);
-      // console.log(response);
+  useEffect(() => {
 
-    })
-    .catch(function(error){
+  async function showAlbums(data) {
+
+    try {
+      const response = await getAlbum('/albums');
+      const albums = response.data.data.map((album) => (
+        <div key={album._id}> 
+          <h3>{album.title}</h3>
+          <h4>{album.description}</h4>
+          <img width='50px' height='50px' src={album.image.img_link}/>
+        </div>
+      ));
+      setAlbum(albums);
+    } catch (error) {
       alert('Albums is not found');
-    })
+    }
   }
-  showAlbums()
+  showAlbums();
+}, []);
+
   return (
     <Container className='photo-container'>
       <Row>
-        <CreatePhoto showAlbums={showAlbums} isAlbum={true}/>
+        <CreatePhoto showAlbums={useEffect} isAlbum={true}/>
         <h3>Albums</h3>
       </Row>
       <Row>
